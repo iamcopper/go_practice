7542adc34e750892d53d7f4801b6728d012e11af
iamcopper <kangpan519@gmail.com>
2019-07-31 11:47:22 +0800

    [go_plugin] add interface type assert statement.

--- go_plugin/main.go
+++ go_plugin/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "plugin"
+import (
+	"fmt"
+	"plugin"
+)
 
 func main() {
 	p, err := plugin.Open("testplugin.so")
@@ -20,4 +23,15 @@ func main() {
 	}
 
 	hello()
+
+	/* Using type switch to check the interface type */
+	switch t := f.(type) {
+	case func():
+		fmt.Printf("It's a func() type, type=%T value=%v\n", t, t)
+	case int:
+		fmt.Printf("It's a int type, type=%T value=%v\n", t, t)
+	default:
+		fmt.Printf("It's a unknown type, type=%T, value=%v\n", t, t)
+	}
+
 }
