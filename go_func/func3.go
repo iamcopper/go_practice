f18186ec37eefa518cb96fde80916c8abd4b4424
iamcopper <kangpan519@gmail.com>
2019-08-17 17:10:19 +0800

    [go_func:func3] add func3.go, function value example

---
+++ go_func/func3.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//定义一种类型-函数类型
+type Caculate func(a, b int) int
+
+func add(a, b int) int {
+	return a + b
+}
+
+func main() {
+	//声明函数类型变量, 并将add函数赋值给函数类型变量
+	//注意类型匹配
+	var caculate Caculate = add
+	//caculate := add
+
+	fmt.Printf("add(1, 2) = %d\n", add(1, 2))
+	fmt.Printf("caculate(1, 2) = %d\n", caculate(1, 2))
+
+	fmt.Printf("\n\"add\" type: %T\n", add)
+	fmt.Printf("\"caculate\" type: %T\n", caculate)
+}
