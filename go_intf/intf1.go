c321650e5f9da584f8e4b5e8850d7e64206caf8a
iamcopper <kangpan519@gmail.com>
2019-08-18 23:57:31 +0800

    [go_intf:intf1] add interface example 1

---
+++ go_intf/intf1.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type Shaper interface {
+	Area() float32
+}
+
+type Square struct {
+	side float32
+}
+
+func (sq *Square) Area() float32 {
+	return sq.side * sq.side
+}
+
+func main() {
+	sq1 := new(Square)
+	sq1.side = 5
+
+	// var areaIntf Shaper
+	// areaIntf = sq1
+
+	// areaIntf := Shaper(sq1)
+
+	areaIntf := sq1
+}
