466f30526bc909e5fc54ca3299b9f4a6559ae3fd
iamcopper <kangpan519@gmail.com>
2019-08-20 09:33:05 +0800

    [go_func:func1_array] remove array pointer example in func1_array.go

--- go_func/func1_array.go
+++ go_func/func1_array.go
@@ -1,4 +1,4 @@
-//slice
+//array
 package main
 
 import (
@@ -13,25 +13,14 @@ func main() {
 
 	fmt.Println("[out]", arr)
 
-	modifyArray1(arr)
-	fmt.Println("[out]", arr)
-
-	modifyArray2(&arr)
+	modifyArray(arr)
 	fmt.Println("[out]", arr)
 }
 
-func modifyArray1(a [10]int) {
+func modifyArray(a [10]int) {
 	a[0] = 11
 	a[1] = 12
 	a[2] = 13
 	a[3] = 14
 	fmt.Println("[in] ", a)
-}
-
-func modifyArray2(aptr *[10]int) {
-	(*aptr)[0] = 11
-	(*aptr)[1] = 12
-	(*aptr)[2] = 13
-	(*aptr)[3] = 14
-	fmt.Println("[in] ", *aptr)
 }
